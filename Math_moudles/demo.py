ab7db95d27330a2b38838f7e12c8160ea35c4571
juwenzhang <3137536916@qq.com>
2024-11-08 23:05:17 +0800

    feat: upload README.md and math moudles basic use

---
+++ Math_moudles/demo.py
@@ -0,0 +1,10 @@
+import math
+
+num = 3.14336532656324
+
+print(f"一个数字向上取整为:{math.ceil(num)}")
+print(f"一个数字向下取证为:{math.floor(num)}")
+print(f"一个数字取绝对值的浮点数为:{math.fabs(num)}")
+print(f"对一个列表中的每个元素求和为: {math.fsum([1.11, 2, 43.132423])}")
+print(f"{8}的{3}次分为: {math.pow(8, 3)}")
+print(f"9的开放为{math.sqrt(9)}")

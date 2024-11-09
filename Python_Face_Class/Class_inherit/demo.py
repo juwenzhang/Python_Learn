0ecd8e6e05a9544132ef93c8551ffb327e5b73e1
juwenzhang <3137536916@qq.com>
2024-11-09 20:26:25 +0800

    feat: upload README.md and class inherit use *2

--- Python_Face_Class/Class_inherit/demo.py
+++ Python_Face_Class/Class_inherit/demo.py
@@ -52,12 +52,43 @@ class Person(object):
 # 开始定义一个类来继承我们的 Person 类
 class Student(Person):
 
+    def __init__(self, name, age, addr, *args, **kwargs):
+        """
+
+        :param name:
+        :param age:
+        :param addr:
+        :param args:
+        :param kwargs:
+        """
+        # 实现的是显示调用我们的父类中的方法
+        # Person.__init__(self, name, age, *args, **kwargs)
+
+        # 开始实现隐式的调用父类中的方法
+        super().__init__(name, age, *args, **kwargs)
+        self.addr = addr
+
     def studing(self, *args, **kwargs):
+        """
+        :param args:
+        :param kwargs:
+        :return:
+        """
         print("我正在学习")
+
+    def running(self, *args, **kwargs):
+        """
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        # super().running(self)
+        Person.running(self)
+        print(f"我在{self.addr}")
 
 
 if __name__ == "__main__":
-    stu = Student("76433", 18)
+    stu = Student("76433", 18, "university_post")
     # 直接实现调用父类中的方法
     print(stu.name)
     print(stu.num)
